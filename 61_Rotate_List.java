7edc49482bf76e25a21d0ee35074a17e0244b4ae
SudhanshuChavhan <78977275+SudhanshuChavhan@users.noreply.github.com>
2022-03-11 23:21:45 +0530

    Day 70

---
+++ 61_Rotate_List.java
@@ -0,0 +1,48 @@
+/*
+61. Rotate List
+Given the head of a linked list, rotate the list to the right by k places.
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Example 2:
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+Constraints:
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 10^9
+*/
+class Solution {
+    public ListNode rotateRight(ListNode head, int n) {
+        if (head==null||head.next==null) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode fast=dummy,slow=dummy;
+
+        int i;
+        for (i=0;fast.next!=null;i++)//Get the total length 
+            fast=fast.next;
+
+        for (int j=i-n%i;j>0;j--) //Get the i-n%i th node
+            slow=slow.next;
+
+        fast.next=dummy.next; //Do the rotation
+        dummy.next=slow.next;
+        slow.next=null;
+
+        return dummy.next;
+    }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
